4bb41e80588759fd454486806ecffff1d4f54ae7
Vadim <vadimka.kashapov.02@mail.ru>
2024-03-11 00:53:51 +0500

    add_crete_tables_file

---
+++ migrations/create_tables.sql
@@ -0,0 +1,17 @@
+create table if not exists chat (
+    chat_id bigint generated always as identity,
+    created_at timestamp with time zone not null,
+    edited_at timestamp with time zone not null,
+
+    primary key(chat_id)
+);
+
+create table if not exists links (
+    link_id bigint generated always as identity,
+    link varchar not null,
+    chat_id bigint not null,
+    created_at timestamp with time zone not null,
+    edited_at timestamp with time zone not null,
+
+    foreign key (chat_id) references chat(chat_id)
+);
