59d69bb33c44639747c31c52d44b79ffafb35cd9
Vadim <vadimka.kashapov.02@mail.ru>
2024-03-11 20:56:54 +0500

    integration_test_edited

--- scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
@@ -1,10 +1,24 @@
 package edu.java.scrapper;
 
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import liquibase.resource.DirectoryResourceAccessor;
+import org.junit.jupiter.api.Test;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.io.File;
+import java.sql.DriverManager;
+
+import static org.junit.Assert.assertTrue;
 
 @Testcontainers
 public abstract class IntegrationTest {
@@ -20,8 +34,28 @@ public abstract class IntegrationTest {
         runMigrations(POSTGRES);
     }
 
-    private static void runMigrations(JdbcDatabaseContainer<?> c) {
-        // ...
+    private static void runMigrations(JdbcDatabaseContainer<?> c)  {
+        try (var connection = DriverManager.getConnection(c.getJdbcUrl(), c.getUsername(), c.getPassword())) {
+            Database database =
+                    DatabaseFactory.getInstance().findCorrectDatabaseImplementation(
+                            new JdbcConnection(connection));
+            var changelogPath =
+                    new File(".")
+                            .toPath()
+                            .toAbsolutePath()
+                            .getParent()
+                            .getParent()
+                            .resolve("migrations");
+            Liquibase liquibase =
+                    new liquibase
+                            .Liquibase("master.xml",
+                            new DirectoryResourceAccessor(changelogPath),
+                            database);
+
+            liquibase.update(new Contexts(), new LabelExpression());
+        } catch (Exception e) {
+            throw new RuntimeException("Migration error", e);
+        }
     }
 
     @DynamicPropertySource
@@ -29,5 +63,10 @@ public abstract class IntegrationTest {
         registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
         registry.add("spring.datasource.username", POSTGRES::getUsername);
         registry.add("spring.datasource.password", POSTGRES::getPassword);
+    }
+
+    @Test
+    void testThatContainerIsCreatedReturnedSucceed() {
+        assertTrue(POSTGRES.isCreated());
     }
 }
